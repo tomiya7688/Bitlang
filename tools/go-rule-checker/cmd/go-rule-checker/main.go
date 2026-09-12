ea89e928fce4b12f7e7b5a6dc81e7f5d7745e83e
kadoka <tomiya3499@gmail.com>
2026-09-12 12:52:05 +0900

    Wire Go rule checker command

--- tools/go-rule-checker/cmd/go-rule-checker/main.go
+++ tools/go-rule-checker/cmd/go-rule-checker/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	gorulechecker "github.com/tomiya7688/Bitlang/tools/go-rule-checker"
+)
+
+// main starts the Go implementation rule checker and maps its result to an exit status.
+func main() {
+	code, err := gorulechecker.Run(os.Args[1:], os.Stdout)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	os.Exit(code)
+}
