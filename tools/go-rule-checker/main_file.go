56f1a0afa4f468223ce25a84f4ff69ca3e9fac4d
kadoka <tomiya3499@gmail.com>
2026-09-12 12:51:32 +0900

    Add main file rule checks

---
+++ tools/go-rule-checker/main_file.go
@@ -0,0 +1,31 @@
+package gorulechecker
+
+import (
+	"go/ast"
+	"go/token"
+	"path/filepath"
+)
+
+func checkMainFile(fileSet *token.FileSet, file *ast.File, path string) []finding {
+	if filepath.Base(path) != "main.go" {
+		return nil
+	}
+
+	var findings []finding
+	for _, declaration := range file.Decls {
+		function, ok := declaration.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+		if function.Name.Name == "main" {
+			continue
+		}
+		findings = append(findings, finding{
+			level:   "WARN",
+			path:    path,
+			line:    declarationLine(fileSet, function.Name),
+			message: "main.go should contain startup logic only; move function \"" + function.Name.Name + "\" to its responsibility file",
+		})
+	}
+	return findings
+}
