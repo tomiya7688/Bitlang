c83bec9ba8ea629c11f8ffdf695a3dc1921bad0b
kadoka <tomiya3499@gmail.com>
2026-09-12 12:53:54 +0900

    Split Go source check responsibility

---
+++ tools/go-rule-checker/source_check.go
@@ -0,0 +1,27 @@
+package gorulechecker
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+func inspectFile(path string) ([]finding, error) {
+	fileSet := token.NewFileSet()
+	parsed, err := parser.ParseFile(fileSet, path, nil, parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+
+	var findings []finding
+	findings = append(findings, checkFilename(path)...)
+	findings = append(findings, checkNames(fileSet, parsed, path)...)
+	findings = append(findings, checkDocumentation(fileSet, parsed, path)...)
+	findings = append(findings, checkFunctionSizes(fileSet, parsed, path)...)
+	findings = append(findings, checkMainFile(fileSet, parsed, path)...)
+	return findings, nil
+}
+
+func declarationLine(fileSet *token.FileSet, node ast.Node) int {
+	return fileSet.Position(node.Pos()).Line
+}
