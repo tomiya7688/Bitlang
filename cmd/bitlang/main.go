8f5d41ea5da1c7667ae772d7c83e9b1aae940262
kadoka <tomiya3499@gmail.com>
2026-09-15 17:51:14 +0900

    Keep main limited to startup and exit mapping

--- cmd/bitlang/main.go
+++ cmd/bitlang/main.go
@@ -1,39 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	core "github.com/tomiya7688/Bitlang/internal/bitlang"
+	"github.com/tomiya7688/Bitlang/internal/cui"
 )
 
-func usage() {
-	fmt.Fprintln(os.Stderr, "usage:")
-	fmt.Fprintln(os.Stderr, "  bitlang canonicalize <identifier> [identifier ...]")
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-		os.Exit(2)
-	}
-
-	switch os.Args[1] {
-	case "canonicalize":
-		if len(os.Args) < 3 {
-			usage()
-			os.Exit(2)
-		}
-		for _, identifier := range os.Args[2:] {
-			canonical, err := core.CanonicalizeIdentifier(identifier)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
-			}
-			fmt.Printf("%s\t%s\n", identifier, canonical)
-		}
-	default:
-		usage()
-		os.Exit(2)
-	}
+	os.Exit(cui.Run(os.Args[1:], os.Stdout, os.Stderr))
 }
