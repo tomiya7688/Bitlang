2b69133b92476dcf36711dd0910340f830426c3f
kadoka <tomiya3499@gmail.com>
2026-09-15 17:54:31 +0900

    Document exported artifact kind constants

--- internal/bitlang/artifact_kind.go
+++ internal/bitlang/artifact_kind.go
@@ -3,6 +3,7 @@ package bitlang
 // ArtifactKind identifies one canonical Bitlang pipeline representation.
 type ArtifactKind string
 
+// Canonical Bitlang pipeline artifact kinds.
 const (
 	ArtifactSource       ArtifactKind = "source"
 	ArtifactPreprocessed ArtifactKind = "preprocessed"
