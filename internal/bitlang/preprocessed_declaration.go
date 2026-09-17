40a5d214c43ef3c9e7beb2f09086b36d57eb05d4
kadoka <tomiya3499@gmail.com>
2026-09-17 18:03:25 +0900

    Add strict preprocessed declaration representation

---
+++ internal/bitlang/preprocessed_declaration.go
@@ -0,0 +1,12 @@
+package bitlang
+
+// PreprocessedDeclaration represents one declaration after preprocessing.
+// Every property axis applicable to the declaration must be present explicitly;
+// later compiler stages must not reconstruct omitted defaults.
+type PreprocessedDeclaration struct {
+	Name       CanonicalName
+	Type       CanonicalName
+	Properties []PreprocessedProperty
+	Line       int
+	Column     int
+}
