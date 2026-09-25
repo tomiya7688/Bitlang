f26a007721716680a7434a4b449ebbce2c7f1b4e
kadoka <tomiya3499@gmail.com>
2026-09-25 20:12:38 +0900

    Load and validate Bitlang specification set once

---
+++ internal/bitlang/specification_set_loader.go
@@ -0,0 +1,21 @@
+package bitlang
+
+// LoadSpecificationSet decodes all machine-readable specifications needed by
+// the bootstrap semantic pipeline and validates their cross-file invariants.
+func LoadSpecificationSet(propertiesData []byte, declarationsData []byte) (SpecificationSet, error) {
+	properties, err := LoadPropertySpecification(propertiesData)
+	if err != nil {
+		return SpecificationSet{}, err
+	}
+	declarations, err := LoadDeclarationSpecification(declarationsData)
+	if err != nil {
+		return SpecificationSet{}, err
+	}
+	if err := ValidateSpecificationConsistency(properties, declarations); err != nil {
+		return SpecificationSet{}, err
+	}
+	return SpecificationSet{
+		Properties: properties,
+		Declarations: declarations,
+	}, nil
+}
