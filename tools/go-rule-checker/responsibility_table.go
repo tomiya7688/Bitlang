cabab69618e3818fa3d98a02bda6256a33f62c28
kadoka <tomiya3499@gmail.com>
2026-09-25 20:43:20 +0900

    Document safe responsibility table read

--- tools/go-rule-checker/responsibility_table.go
+++ tools/go-rule-checker/responsibility_table.go
@@ -12,7 +12,7 @@ type responsibilityRecord struct {
 }
 
 func loadResponsibilityTable(path string) ([]responsibilityRecord, error) {
-	file, err := os.Open(path)
+	// #nosec G304 -- path is the repository-owned FILE_RESPONSIBILITIES.md selected by the checker.\n	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
